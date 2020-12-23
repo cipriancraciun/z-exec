981e76d156ff1d7989bd8d6f5d55a45d448c31f6
Ciprian Dorin Craciun <ciprian@volution.ro>
2020-12-23 18:39:28 +0200

    [prelude]  Import dependent libraries and other std modules.

--- sources/prelude.rs
+++ sources/prelude.rs
@@ -12,12 +12,28 @@ pub(crate) use crate::settings::*;
 
 
 
+pub(crate) use ::serde;
+pub(crate) use ::serde_derive;
+pub(crate) use ::serde_json;
+
+pub(crate) use ::libc;
+
+pub(crate) mod nix {
+	pub(crate) use nix::Error;
+	pub(crate) use nix::errno::*;
+	pub(crate) use nix::unistd::*;
+}
+
+
+
+
 pub(crate) use ::std::cmp;
 pub(crate) use ::std::collections::btree_map;
 pub(crate) use ::std::collections::btree_set;
 pub(crate) use ::std::collections::hash_map;
 pub(crate) use ::std::collections::hash_set;
 pub(crate) use ::std::env;
+pub(crate) use ::std::error;
 pub(crate) use ::std::ffi;
 pub(crate) use ::std::fmt;
 pub(crate) use ::std::fs;
@@ -26,6 +42,7 @@ pub(crate) use ::std::mem;
 pub(crate) use ::std::ops;
 pub(crate) use ::std::os::unix::ffi as ffi_unix;
 pub(crate) use ::std::os::unix::fs as fs_unix;
+pub(crate) use ::std::os::unix::io as io_unix;
 pub(crate) use ::std::path;
 pub(crate) use ::std::process;
 
@@ -36,6 +53,9 @@ pub(crate) use ::std::collections::btree_map::BTreeMap;
 pub(crate) use ::std::collections::btree_set::BTreeSet;
 pub(crate) use ::std::collections::hash_map::HashMap;
 pub(crate) use ::std::collections::hash_set::HashSet;
+pub(crate) use ::std::convert::Infallible;
+pub(crate) use ::std::ffi::CStr;
+pub(crate) use ::std::ffi::CString;
 pub(crate) use ::std::ffi::OsStr;
 pub(crate) use ::std::ffi::OsString;
 pub(crate) use ::std::ops::Bound;
